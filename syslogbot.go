24f55120931ae55c09a17b93821506966f0d6a40
joe <presbrey+git@gmail.com>
2013-09-29 02:40:56 -0400

    gofmt

--- syslogbot.go
+++ syslogbot.go
@@ -1,26 +1,25 @@
 package main
 
 import (
-    "log"
-    "os"
-    "os/signal"
-    "syscall"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
-
-    "github.com/ziutek/syslog"
-    "github.com/presbrey/go/syslogbot/lib"
+	"github.com/presbrey/go/syslogbot/lib"
+	"github.com/ziutek/syslog"
 )
 
 func main() {
-    srv := syslogbot.NewBot(os.Args[1])
-    s := syslog.NewServer()
-    s.AddHandler(srv.SyslogHandler())
-    s.Listen("0.0.0.0:514")
-    s.Listen("0.0.0.0:1514")
-    sc := make(chan os.Signal, 2)
-    signal.Notify(sc, syscall.SIGTERM, syscall.SIGINT)
-    <-sc
-    log.Println("Shutdown the server...")
-    s.Shutdown()
-    log.Println("Server is down")
+	srv := syslogbot.NewBot(os.Args[1])
+	s := syslog.NewServer()
+	s.AddHandler(srv.SyslogHandler())
+	s.Listen("0.0.0.0:514")
+	s.Listen("0.0.0.0:1514")
+	sc := make(chan os.Signal, 2)
+	signal.Notify(sc, syscall.SIGTERM, syscall.SIGINT)
+	<-sc
+	log.Println("Shutdown the server...")
+	s.Shutdown()
+	log.Println("Server is down")
 }
